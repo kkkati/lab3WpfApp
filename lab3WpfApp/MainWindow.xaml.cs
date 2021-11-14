3aa633f15f3824e9f7c88574f192b98ba2ab31f2
Екатерина Горяинова <kateas27@mail.ru>
2021-11-14 18:08:38 +0200

    Закончила задание

--- lab3WpfApp/MainWindow.xaml.cs
+++ lab3WpfApp/MainWindow.xaml.cs
@@ -34,5 +34,77 @@ namespace lab3WpfApp
                 textBox.FontFamily = new FontFamily(fontName);
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontStyle == FontStyles.Normal)
+                {
+                    textBox.FontStyle = FontStyles.Italic;
+                }
+                else
+                {
+                    if (textBox.FontStyle == FontStyles.Italic)
+                    {
+                        textBox.FontStyle = FontStyles.Normal;
+                    }
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.FontWeight == FontWeights.Normal)
+                {
+                    textBox.FontWeight = FontWeights.Bold;
+                }
+                else
+                {
+                    if (textBox.FontWeight == FontWeights.Bold)
+                    {
+                        textBox.FontWeight = FontWeights.Normal;
+                    }
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                if (textBox.TextDecorations == null)
+                {
+                    textBox.TextDecorations = TextDecorations.Underline;
+                }
+                else
+                {
+                    if (textBox.TextDecorations == TextDecorations.Underline)
+                    {
+                        textBox.TextDecorations = null;
+                    }
+                }
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                var fg = new BrushConverter();
+                textBox.Foreground = (Brush)fg.ConvertFrom("#FF000000");
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                var fg = new BrushConverter();
+                textBox.Foreground = (Brush)fg.ConvertFrom("#FFF50D0D");
+            }
+        }
     }
 }
